3d2c4ec63d0c33bd0be4bfa42bab56838e00947b
nalouddin <nalouddin@gmail.com>
2025-12-25 12:38:10 +0500

    adminkaga jazzmin qo'shildi

--- core/settings.py
+++ core/settings.py
@@ -33,6 +33,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'jazzmin',
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -139,3 +141,53 @@ MESSAGE_TAGS = {
 
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 CRISPY_TEMPLATE_PACK = "bootstrap5"
+
+
+JAZZMIN_SETTINGS = {
+    'site_brand': 'HMS',
+    'copyright': 'All Rights Reserved 2026',
+    'welcome_sign': 'Welcome to HMS, Login Now',
+
+    'order_with_respect_to': [
+        'base',
+    ],
+
+    'icons': {
+        'admin.LogEntry': 'fas fa-file',
+        'auth': 'fas fa-user-cog',
+        'auth.user': 'fas fa-user',
+    },
+    'show_ui_builder': True,
+}
+
+JAZZMIN_UI_TWEAKS = {
+    "navbar_small_text": False,
+    "footer_small_text": False,
+    "body_small_text": True,
+    "brand_small_text": False,
+    "brand_colour": False,
+    "accent": "accent-primary",
+    "navbar": "navbar-dark",
+    "no_navbar_border": False,
+    "navbar_fixed": False,
+    "layout_boxed": False,
+    "footer_fixed": False,
+    "sidebar_fixed": False,
+    "sidebar": "sidebar-dark-primary",
+    "sidebar_nav_small_text": False,
+    "sidebar_disable_expand": False,
+    "sidebar_nav_child_indent": False,
+    "sidebar_nav_compact_style": False,
+    "sidebar_nav_legacy_style": False,
+    "sidebar_nav_flat_style": False,
+    "theme": "darkly",
+    "dark_mode_theme": None,
+    "button_classes": {
+        "primary": "btn-primary",
+        "secondary": "btn-secondary",
+        "info": "btn-info",
+        "warning": "btn-warning",
+        "danger": "btn-danger",
+        "success": "btn-success"
+    }
+}
